fe590dcaf3cef84c49d4a88c41a4035623cddd7c
NateDogg <natmays97@gmail.com>
2022-08-07 02:54:15 -0500

    finishing touches

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WeatherDashboard
 This is Challenge 06. It is a website that displays the weather for any searchable city up to five days in the future and also stores a brief search history
+
+For this challenge I used a combination of the OneCall API and the Geocoding API to find the location of the user input. I used a fetch request to use the data to dynamically populate the daily forecast up to five days in the future.
+
+I was able to build a seach history bar that utilizes local storage.
+
+I was able to style the website using a combination of bootstrap and CSS.
+
+I was able to build the functionality with a combination of Jquery and JavaScript.
+
+I used moment.js to convert the time fetched from the weather API to a readable format.
+
+The deployed website is: https://natedogg97.github.io/WeatherDashboard/
+
+Screenshot is included in the Github files.
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ var todaysUVIndex = document.querySelector('.todaysUVIndex');
 
 button.addEventListener('click', btnGO);
 
+// This function handles the search button. It stores the value to local storage and starts
+// the fetch requests
+
 function btnGO() {
        
     var locationName = String(userInput.value);
@@ -19,53 +22,55 @@ function btnGO() {
     storeHistory(locationName);
 };
 
+// This function gets the longitude and latitude of the most popular city that matches
+// the search value
+
 function geocodingAPI(locationName) {
     var LONGLATurl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + locationName + '&limit=1&appid=7bdaf7d35d6463a5a9236b225cf80e4a';
 
-    //Write our fetch request function as an expression
     const geoData = fetch(LONGLATurl)
 
         .then(function (response) {
             return response.json();
         })
 
-        //We now have the LATITUDE and LOGITUDE of our city
         .then((data) => {
-            console.log(data); //See the data from the Geocoding API
+            console.log(data);
             return [data[0].lat, data[0].lon];
         });
 
-    //Takes those Coords and passes those coords to the current weather API
     const giveLongLats = () => {
         geoData.then((a) => {
             currentAPI(a[0], a[1]);
         });
     };
 
-    //Fires off the giveLongLats func
     giveLongLats();
 };
+
+// This function is separated for organization. It simply takes the latitude and longitude
+// and return weather data that I can use later on
 
 function currentAPI(Lat, Lon) {
 
     var currentURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + Lat + '&lon=' + Lon + '&exclude=minutely,hourly,alerts&appid=7bdaf7d35d6463a5a9236b225cf80e4a';
 
-    //Makes the fetch current weather function an expression that is manipulatable
     const cityWeather = fetch(currentURL)
 
         .then(function (response) {
             return response.json();
         })
 
-        //We now have the current weather data as the variable 'data'
         .then(function (data) {
-            console.log(data); //See data from the OneCall API
+            console.log(data);
             
-            fillBlanks(data);
-            fillCards(data);
+            fillBlanks(data); // Here I am calling for the page to be populated
+            fillCards(data);  // ----------------------------------------------
         });
 
 };
+
+// This function fills the main section with todays weather data and also handles the UV index
 
 function fillBlanks(data) {
 
@@ -78,7 +83,9 @@ function fillBlanks(data) {
     todaysHumid.textContent = data.current.humidity + ' %';
     todaysDate.textContent = a.charAt(0).toUpperCase() + a.slice(1) + ' ' + moment(data.current.dt).format('dddd, MMMM Do');
     $('#img').attr('src', displayImg(data.current.weather.icon));
-    
+
+// This conditional handles the UV Index color    
+
     if (data.current.uvi < 3){
         todaysUVIndex.textContent = data.current.uvi;
         $('.todaysUVIndex').attr('background-color', 'green')
@@ -94,9 +101,14 @@ function fillBlanks(data) {
     }
 }
 
+// A simple function that helps me convert from kelvin to Fahrenheit
+
 function kelvinToF(number) {
     return 1.8 * (number - 273) + 32
 }
+
+// Here I use Jquery to keep this portion clean. I have to use multiple loops to fill 
+// each <li> with the correct data
 
 function fillCards(data) {
 
@@ -116,11 +128,16 @@ function fillCards(data) {
         $(this).attr('src', displayImg(data.daily[i+1].weather.icon));
     })
 
+// This part was tricky because I had to use the add.() method through moment.js to display
+// the dates properly
+
     $('.card-title').each(function(i){
         ($(this).text(moment(data.daily.dt).add(i+1, 'day').format('ddd, MMM Do')));
     })
 
 }
+
+// This function handles the icon using conditionals
 
 function displayImg(icon) {
     
@@ -153,6 +170,9 @@ function displayImg(icon) {
     return img
 }
 
+// Here I am creating an array and populating it with the values stored in the 'city' key.
+// I can run a few conditionals to limit the seach history. This bit could be improved.
+
 function storeHistory(input){
 
     var a = [];
@@ -169,6 +189,8 @@ function storeHistory(input){
     localStorage.setItem('city', JSON.stringify(a));
 
 }
+
+// Here I use a for loop to create the search history buttons by accessing local storage
 
 function buildHistoryButtons() {
 
@@ -192,7 +214,11 @@ function buildHistoryButtons() {
 
 buildHistoryButtons();
 
+// Here I needed to create a seperate section to make sure the dynamically created buttons were
+// functioning properly
+
 const historyButtons = document.querySelector('#historyBtns')
+
 historyButtons.addEventListener('click', function(e){
     if (e.target.classList.contains('btn-secondary')){
         userInput.value = e.target.name;
